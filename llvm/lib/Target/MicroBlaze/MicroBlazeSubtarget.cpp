e47f145c0980a7984f48945a508076bba5b20162
Samuel Price <thesamprice@gmail.com>
2026-06-24 21:05:28 -0400

    [MicroBlaze][7/65] Add instruction formats and core infrastructure
    Adds the instruction format base classes (Type A register/register, Type B
    register/immediate) and the core support machinery needed by all instructions:
    SDNodes for call-sequence and custom lowering, operand types, address-mode
    ComplexPatterns, call-frame pseudos, NOP, and the IMM prefix instruction.

--- llvm/lib/Target/MicroBlaze/MicroBlazeSubtarget.cpp
+++ llvm/lib/Target/MicroBlaze/MicroBlazeSubtarget.cpp
@@ -20,6 +20,6 @@ MicroBlazeSubtarget::MicroBlazeSubtarget(const Triple &TT, StringRef CPU,
                                          StringRef FS,
                                          const MicroBlazeTargetMachine &TM)
     : MicroBlazeGenSubtargetInfo(TT, CPU, /*TuneCPU=*/CPU, FS),
-      FrameLowering(*this) {
+      InstrInfo(*this), FrameLowering(*this) {
   ParseSubtargetFeatures(CPU, CPU, FS);
 }
